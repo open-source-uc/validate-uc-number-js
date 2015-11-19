d7af4ddd32c9607c557f9ad6ca1b248a1e653eba
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2015-11-19 20:54:19 -0300

    Initial algorithm commit

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-module.exports = function validate(numeroAlmno) {
-  if (numeroAlmno.length == 8) {
-    return true;
-  } else {
-    return false;
+module.exports = function validate(studentNumber) {
+  const givenDigit = studentNumber.split('').reverse().slice(0, 1).join('').toUpperCase();
+  const chars = studentNumber.split('').reverse().splice(1);
+
+  let counter = 2;
+  let sum = 0;
+  for (let char of chars) {
+    sum += Number(char) * counter++;
+    counter = counter > 8 ? 2 : counter;
   }
+
+  const verificationDigit = (11 - sum % 11 != 10) ? String(11 - sum % 11) : 'J';
+  return verificationDigit === givenDigit;
 }
