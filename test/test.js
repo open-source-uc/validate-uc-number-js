aa024c7c85f2cac1d0bdffa8403288c963de86a6
Patricio López <lopezjuripatricio@gmail.com>
2015-11-19 21:03:40 -0300

    add test

--- test/test.js
+++ test/test.js
@@ -7,6 +7,14 @@ const validador = require('../index');
 
 describe('uc-numero-alumno', () => {
   it('should validate valid number', () => {
+    validador('14644088').should.be.true;
+  });
 
+  it('should validate valid number with "J"', () => {
+    validador('1263476J').should.be.true;
+  });
+
+  it('should fail on invalid number', () => {
+    validador('12635662').should.be.false;
   });
 });
