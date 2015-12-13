3ee1673a0804d1b78029b8191abbe4422ba6e19f
Patricio López <lopezjuripatricio@gmail.com>
2015-12-13 11:23:32 -0300

    make ES5 compatible

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-'use strict';
-
 /**
  * Validate student number identifier.
- * @param  {(string|number)} studentNumber Student ID.
- * @return {boolean}               Return true if the input is valid, false otherwise.
+ * @param  {(string|number)} student Number Student ID.
+ * @return {boolean}                 Return true if the input is valid, false otherwise.
  */
-module.exports = function validate(studentNumber) {
+function validate(studentNumber) {
   if (studentNumber === undefined) {
     return false;
   }
 
   studentNumber = String(studentNumber);
-  const givenDigit = studentNumber.split('').reverse().slice(0, 1).join('').toUpperCase(),
+  var givenDigit = studentNumber.split('').reverse().slice(0, 1).join('').toUpperCase(),
     chars = studentNumber.split('').reverse().splice(1);
 
-  let counter = 2,
+  var counter = 2,
     sum = 0;
-  for (let char of chars) {
-    sum += Number(char) * counter++;
+  for (var i = 0; i < chars.length; i++) {
+    sum += Number(chars[i]) * counter++;
     counter = counter > 8 ? 2 : counter;
   }
 
-  const verificationDigit = (11 - sum % 11 != 10) ? String(11 - sum % 11) : 'J';
+  var verificationDigit = (11 - sum % 11 != 10) ? String(11 - sum % 11) : 'J';
   return verificationDigit === givenDigit;
 }
+
+module.exports = validate
